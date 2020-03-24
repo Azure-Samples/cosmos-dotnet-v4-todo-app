e938aeb96f4a029263609fba4f7b7703d51e0ee7
Kalyan Chanumolu <kachanum@microsoft.com>
2020-03-24 09:30:54 +0530

    MVC Sample for CosmosV4 API

---
+++ src/Models/Item.cs
@@ -0,0 +1,19 @@
+﻿namespace todo.Models
+{
+    using System.Text.Json.Serialization;
+
+    public class Item
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
+        [JsonPropertyName("isComplete")]
+        public bool Completed { get; set; }
+    }
+}
---
+++ src/Services/CosmosDbService.cs
@@ -0,0 +1,59 @@
+﻿namespace todo.Services
+{
+    using Azure.Cosmos;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using todo.Models;
+
+    public class CosmosDbService : ICosmosDbService
+    {
+        private readonly CosmosContainer _cosmosContainer;
+
+        public CosmosDbService(CosmosClient dbClient, string databaseName, string containerName)
+        {
+            this._cosmosContainer = dbClient.GetContainer(databaseName, containerName);
+        }
+
+        public async Task AddItemAsync(Item item)
+        {
+            await this._cosmosContainer.CreateItemAsync<Item>(item, new PartitionKey(item.Id));
+        }
+
+        public async Task DeleteItemAsync(string id)
+        {
+            await this._cosmosContainer.DeleteItemAsync<Item>(id, new PartitionKey(id));
+        }
+
+        public async Task<Item> GetItemAsync(string id)
+        {
+            try
+            {
+                ItemResponse<Item> response = await this._cosmosContainer.ReadItemAsync<Item>(id, new PartitionKey(id));
+                return response;
+            }
+            catch (CosmosException ex) when (ex.Status == (int)System.Net.HttpStatusCode.NotFound)
+            {
+                return null;
+            }
+        }
+
+        public async Task<IEnumerable<Item>> GetItemsAsync(string queryString)
+        {
+            QueryDefinition queryDefinition = new QueryDefinition(queryString);
+
+            List<Item> results = new List<Item>();
+
+            await foreach (Item item in _cosmosContainer.GetItemQueryIterator<Item>(queryDefinition))
+            {
+                results.Add(item);
+            }
+
+            return results;
+        }
+
+        public async Task UpdateItemAsync(Item item)
+        {
+            await this._cosmosContainer.UpsertItemAsync<Item>(item, new PartitionKey(item.Id));
+        }
+    }
+}
